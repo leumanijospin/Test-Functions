daea815f956cab7b13a6ceca9964ee7dcd545d99
leumani jospin <jospinunifer@gmail.com>
2022-08-10 12:48:03 +0100

    new refactoring code

--- functions/allFunctions.ts
+++ functions/allFunctions.ts
@@ -1,15 +1,15 @@
-export {sumTwoSmallerNumberFrom, isCommentReallySafe, areAllUsersSafed, guessTheCorrectNumberEnterByUser, getLongestStringFrom, getFilterValueOfarray, getMusicListInArrayFrom, deletePropertyFrom, getMostFrequentElementFrom, getUniquelyElementInArray, getFlattenOfArray, addElementsInEndFrom, getVolumeOfCylinder, getMultiplication, removeRedundantCharacters, getExtractOfValuesIndexes, getAdd, getLargestEvenNumberIn, sumOfCubedNumberUpTo, getUniqueElementsInSingleArray, bothObjectsAreEquals};
+export {sumTwoSmallerNumberFrom, isCommentReallySafe, areAllUsersSafed, guessTheCorrectNumberEnterByUser, getLongestStringFrom, getFilter, getMusicListFrom, deletePropertyFrom, getMostFrequentElementFrom, getUniquelyElement, getFlatten, addElementsInEndFrom, getVolumeCylinder, multiplication, removeRedundantCharacters, getExtractValuesIndexes, getSum, getLargestEvenNumber, sumOfCubedNumberUpTo, getUniqueElements, bothObjectsAreEquals};
 
-const  getVolumeOfCylinder = (radius: number, height:number): number => Number((2 * radius * height * Math.PI).toFixed(4));
+const  getVolumeCylinder = (radius: number, height:number): number => Number((2 * radius * height * Math.PI).toFixed(4));
   
-const getMultiplication = (firstNumber: number, secondNumber: number): number =>  firstNumber * secondNumber;
+const multiplication = (firstNumber: number, secondNumber: number): number =>  firstNumber * secondNumber;
 
 const removeRedundantCharacters = (word: string): string => [...new Set(word.split(''))].join('');
 
-const getAdd = (firstValue: number, secondValue: number): object => {
+const getSum = (firstNumber: number, secondNumber: number): object => {
     return new Promise ((resolve, reject)=> {
-      if (firstValue && secondValue) {
-       resolve(firstValue + secondValue)
+      if (firstNumber && secondNumber) {
+       resolve(firstNumber + secondNumber)
       }
       else {
          reject('Must provide two parameters') 
@@ -18,7 +18,7 @@ const getAdd = (firstValue: number, secondValue: number): object => {
   }
   
   
-const getExtractOfValuesIndexes = (values: (number | String)[], ...indexes: number[]): (number | String)[] => indexes.map(index => values[index]);
+const getExtractValuesIndexes = (values: (number|string)[], ...indexes: number[]): (number|string)[] => indexes.map(index => values[index]);
 
 const sumOfCubedNumberUpTo = (element: number): number  => {
    let sum = 0;
@@ -29,20 +29,20 @@ const sumOfCubedNumberUpTo = (element: number): number  => {
  }
 
 
-const getLargestEvenNumberIn = (array: number[]): number => Math.max(...array.filter(element => element % 2 === 0))
+const getLargestEvenNumber = (numbers: number[]): number => Math.max(...numbers.filter(element => element % 2 === 0))
 
-const getUniqueElementsInSingleArray = (firstArray: (number | String)[], SecondArray: (number | String)[]): (number | String)[]=> [...new Set (firstArray.concat(SecondArray))]
+const getUniqueElements = (firstArray: (number | string)[], secondArray: (number | string)[]): (number | string)[]=> [...new Set (firstArray.concat(secondArray))]
     
-const addElementsInEndFrom = (array: (Number|String)[], ...elements:(number|String)[]): (Number|String)[] => array.concat(elements).flat(Infinity)
+const addElementsInEndFrom = (elements: (number|string)[], ...elementsToAdd:(number|string)[]): (number|string)[] => elementsToAdd.concat(elements).flat(Infinity)
     
 
-const getFlattenOfArray = (array: (Number|String)[]): (Number|String)[] => array.flat(Infinity)
+const getFlatten = (nestedArray: (number|string)[]): (number|string)[] => nestedArray.flat(Infinity)
 
-const getUniquelyElementInArray = (array:  (Number|String)[]): (Number|String)[] => [...new Set (array)]
+const getUniquelyElement = (elements:  (number|string)[]): (number|string)[] => [...new Set (elements)]
 
 type Element = number | string 
 
-const getMostFrequentElementFrom = (elements: Array<Element>) => elements.sort((firstValue: Element, secondValue:Element) => elements.filter(element => element===firstValue).length- elements.filter(element => element===secondValue).length).pop();
+const getMostFrequentElementFrom = (elements: Array<Element>) => elements.sort((firstValue, secondValue) => elements.filter(element => element===firstValue).length- elements.filter(element => element===secondValue).length).pop();
 
 type Student = {
   name: string;
@@ -66,19 +66,18 @@ type People = {
   music: string[]
 }
 
-const getMusicListInArrayFrom = (peopleList: Array<People>): string[] => [...new Set (peopleList.map(item => item.music).flat())]
+const getMusicListFrom = (peopleList: Array<People>): string[] => [...new Set (peopleList.map(item => item.music).flat())]
 
-const getFilterValueOfarray = (array: (string |number)[], indexes: number[]): (string |number)[] => array.filter(item => !indexes.includes(array.indexOf(item)))
+const getFilter = (values: (string|number)[], indexes: number[]): (string|number)[] => values.filter(item =>!indexes.includes(values.indexOf(item)))
 
-const getLongestStringFrom = (array: string[]): string => {
-  return array.reduce((previousValue, currentValue)=> previousValue.length > currentValue.length ? previousValue : currentValue)
+const getLongestStringFrom = (wordList: string[]): string => {
+  return wordList.reduce((previousValue, currentValue)=> previousValue.length > currentValue.length ? previousValue : currentValue)
 }
 
 const guessTheCorrectNumberEnterByUser = (number: number): string => {
-  let random = Math.floor(Math.random() * 10) + 1;
-  if (number === random){
-      return 'Good Work'
-  }else return 'Not Matched'
+  const random = Math.floor(Math.random() * 10) + 1;
+  if (number === random)return 'Good Work'
+       else return 'Not Matched'
 };
 
 
@@ -89,7 +88,7 @@ type User = {
 
 const areAllUsersSafed = (users: Array<User>): boolean => users.every(item =>item.safe)
 
-const isCommentReallySafe = (word: string, wordList: string[]): boolean =>  wordList.every(element => ! word.includes(element))
+const isCommentReallySafe = (word: string, wordList: string[]): boolean => wordList.every(element =>!word.includes(element))
 
 const sumTwoSmallerNumberFrom = (numbers: Array<number>): number =>  {
        numbers.sort()
